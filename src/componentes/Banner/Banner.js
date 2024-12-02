8386e0b660ea438c52659b59d78fd18f540bca58
Bevilhonda <marcelinyuri@gmail.com>
2024-12-02 09:43:04 -0300

    cria componente Banner e cria arquivo de css para o componente Banner

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,13 @@
+import './Banner.css'
+
+function Banner() {
+  return (
+    <header className="banner">
+      <img src="/imagens/banner.png" className="banner" alt="imagem do banner" />
+
+    </header>
+
+  )
+}
+
+export default Banner
