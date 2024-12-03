1e2e66a13991065f35bd7ae87966095d4357a9d4
Bevilhonda <marcelinyuri@gmail.com>
2024-12-02 21:25:58 -0300

    cria componente campo texto

---
+++ src/componentes/CampoTexto/CampoTexto.js
@@ -0,0 +1,14 @@
+import './CampoTexto.css'
+
+const CampoTexto = () => {
+
+  return(
+    <div className="campo-texto">
+      <label>Nome</label>
+      <input placeholder='Digite seu Nome'/>
+    </div>
+  )
+
+}
+
+export default CampoTexto
