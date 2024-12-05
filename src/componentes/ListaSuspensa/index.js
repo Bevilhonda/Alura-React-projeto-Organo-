19ee1e5b5b0a84f5713ce69dd1ebe08d528021cc
Bevilhonda <marcelinyuri@gmail.com>
2024-12-05 08:35:42 -0300

    atualiza logica para pegar dados do input

---
+++ src/componentes/ListaSuspensa/index.js
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import './ListaSuspensa.css'
+
+const ListaSuspensa = (props) => {
+
+  const [valor, setValor] = useState()
+
+
+  return (
+    <div className='lista-suspensa'>
+      <label>{props.label} </label>
+
+      <select
+        onChange={evento => props.aoAlterado(evento.target.value)}
+        required={props.required}
+        value={props.valor}>
+        {props.itens.map(item => {
+          return <option key={item}>{item}</option>
+        })}
+      </select>
+    </div>
+  )
+
+}
+
+export default ListaSuspensa
